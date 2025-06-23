57ee9d692227d4e49c827cdbe0e7aa0f5fb35994
Adinna04 <thawareadinna04@gmail.com>
2025-06-23 09:35:59 +0530

    excel-analytics-platform deleted

--- backend/excel-analytics-platform/frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="relative z-10 bg-black/80 text-lime-300 border-t border-lime-400 py-4 text-center text-sm">
-      © 2025 ExcelVerse • Crack the Case of Data 🧠🔎
-    </footer>
-  );
-}
